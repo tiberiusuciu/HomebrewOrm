c5560236bb25cee1ff588898d771e2928b1c374b
Tiberiu Cristian Suciu <chrisuciu@yahoo.ca>
2016-11-23 21:07:26 -0500

    Added base methods for our homebrewOrm

--- src/main/java/com/orm/HomebrewOrm.java
+++ src/main/java/com/orm/HomebrewOrm.java
@@ -1,8 +1,12 @@
 package com.orm;
 
+import java.util.ArrayList;
+
 public class HomebrewOrm {
 	
-	public HomebrewOrm instance = null;
+	public HomebrewOrm instance = null;	
+	public ArrayList<String> listeTransactions;
+	
 	
 	private HomebrewOrm() {
 		
@@ -13,6 +17,62 @@ public class HomebrewOrm {
 			instance = new HomebrewOrm();
 		}
 		return instance;
+	}
+	
+	public void insert() {
+		
+	}
+	
+	public void createTable() {
+		
+	}
+	
+	public void updateData() {
+		
+	}
+	
+	public void updateTable() {
+		
+	}
+	
+	public void deleteValue() {
+		
+	}
+	
+	public void removeValue() {
+		
+	}
+	
+	public void removeTable() {
+		
+	}
+	
+	public void commit() {
+		
+	}
+	
+	public void select() {
+		
+	}
+	
+	public void oneToOne() {
+		
+	}
+	
+	public void oneToMany() {
+		
+	}
+	
+	public void manyToMany() {
+		
+	}
+	
+	private void loadData() {
+		
+	}
+	
+	private void writeData() {
+		
 	}
 	
 }
