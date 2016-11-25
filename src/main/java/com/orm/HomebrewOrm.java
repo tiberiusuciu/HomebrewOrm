016881324ced39051bd379d0e2dce77d25d99411
Jean-Daniel Rondeau <jee-dee-07@hotmail.com>
2016-11-25 13:25:45 -0500

    ajout de la fonction updateData

--- src/main/java/com/orm/HomebrewOrm.java
+++ src/main/java/com/orm/HomebrewOrm.java
@@ -1,6 +1,8 @@
 package com.orm;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomebrewOrm {
 	
@@ -21,7 +23,7 @@ public class HomebrewOrm {
 	}
 	
 	public void insert(HomebrewOrmObject object, String tableName) {
-		listeTransactions.add("insert "+object.toHomebrewOrmData()+" into "+ tableName);
+		listeTransactions.add("insert;"+object.toHomebrewOrmData()+";into "+ tableName);
 	}
 	
 	public boolean createTable (HomebrewOrmTable table) {
@@ -34,8 +36,18 @@ public class HomebrewOrm {
 		return flag;
 	}
 	
-	public void updateData() {
-		
+	public void updateData(HashMap<String, String> collumnsToUpdate, 
+						   HashMap<String, String> where) {
+		String transaction = "update;{";
+		for(Map.Entry<String, String> collumn : collumnsToUpdate.entrySet()) {
+			transaction+=collumn.getKey()+":"+collumn.getValue()+",";
+		}
+		transaction+="};where{";
+		for(Map.Entry<String, String> whereValue : where.entrySet()) {
+			transaction+=whereValue.getKey()+":"+whereValue.getValue()+",";
+		}
+		transaction+="}";
+		listeTransactions.add(transaction);
 	}
 	
 	public void updateTable() {
